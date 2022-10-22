12b93fecfaa22e4160927721d7ef93647c80cb26
Anu <anuraj2700@gmail.com>
2022-10-22 22:07:03 +0530

    Edura Initial setup

--- app/src/main/java/com/example/loginactivity/userprofile.java
+++ app/src/main/java/com/example/loginactivity/userprofile.java
@@ -2,7 +2,11 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class userprofile extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class userprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
+    }
+//logout from the application
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
